4b22aff070890301eb5fc659e9fa39708943b342
Jeong Mingyu <tomorrow9913@gmail.com>
2024-11-30 07:01:57 +0000

    feat: parameterize topic in event manager setup and subscriptions for flexibility

--- main.py
+++ main.py
@@ -14,21 +14,22 @@ async def main():
     logger = setup_logging(__name__)
     
     kafka_brokers = os.getenv("KAFKA_BROKERS")
+    topic = os.getenv("KAFKA_TOPIC")
     if not kafka_brokers:
         logger.error("No Kafka brokers configured, skipping event manager setup")
         exit(-1)
         
     logger.info("Initializing event manager")
-    event_manager = EventManager(kafka_brokers, "alert")
+    event_manager = EventManager(kafka_brokers, topic)
     
     logger.info("Subscribing to callbacks")
     for callback in callbacks:
-        logger.info(f"Subscribing {callback} to alert topic")
-        event_manager.subscribe(callback, "alert")
+        logger.info(f"Subscribing {callback} to {topic} topic")
+        event_manager.subscribe(callback, topic)
     
     try:
         logger.info("Starting event manager")
-        await event_manager.start("alert")
+        await event_manager.start(topic)
         while True:
             await asyncio.sleep(1)
     except KeyboardInterrupt:
@@ -36,7 +37,7 @@ async def main():
     finally:
         logger.info("stop event manager")
         if event_manager:
-            await event_manager.stop("alert")
+            await event_manager.stop(topic)
 
 if __name__ == "__main__":
     try:
