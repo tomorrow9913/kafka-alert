0cf631426729fb4b760faa31f5b2a86fcdc86850
Jeong Mingyu <tomorrow9913@gmail.com>
2024-11-30 06:22:28 +0000

    feat: refactor main function to include logging and improve event manager lifecycle handling

--- main.py
+++ main.py
@@ -1,35 +1,45 @@
 import os
+import asyncio
 
 from utils.queue_eventmanager import EventManager
+from utils.logger import setup_logging
 from callback import callbacks
 
 event_manager = None
+logger = None
 
-def lifecycle():
+async def main():
+    global logger
     global event_manager
+    logger = setup_logging(__name__, os.getenv("LOG_DIR", "logs"))
     
     kafka_brokers = os.getenv("KAFKA_BROKERS")
     if not kafka_brokers:
-        print("No Kafka brokers configured, skipping event manager setup")
+        logger.error("No Kafka brokers configured, skipping event manager setup")
         exit(-1)
+        
+    logger.info("Initializing event manager")
     event_manager = EventManager(kafka_brokers, "alert")
+    
+    logger.info("Subscribing to callbacks")
     for callback in callbacks:
+        logger.info(f"Subscribing {callback} to alert topic")
         event_manager.subscribe(callback, "alert")
-    yield
-    if event_manager:
-        event_manager.stop("alert")
-
-async def main():
-    next(lifecycle())
+    
     try:
+        logger.info("Starting event manager")
         await event_manager.start("alert")
         while True:
-            pass
+            await asyncio.sleep(1)
     except KeyboardInterrupt:
-        pass
+        logger.info("Shutting down event manager")
     finally:
-        next(lifecycle())
+        logger.info("stop event manager")
+        if event_manager:
+            await event_manager.stop("alert")
 
 if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
+    try:
+        asyncio.get_event_loop().run_until_complete(main())
+    except KeyboardInterrupt:
+        pass
