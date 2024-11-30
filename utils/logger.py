8c3f8693848c5f06284b7dc6bb2817b36fc028e4
Jeong Mingyu <tomorrow9913@gmail.com>
2024-11-30 06:16:43 +0000

    feat: add logging setup utility with rotating file handler

---
+++ utils/logger.py
@@ -0,0 +1,27 @@
+import os
+import logging
+
+from logging.handlers import RotatingFileHandler
+
+def setup_logging(logger_name: str, log_dir: str = os.getenv("LOG_DIR", "logs")) -> logging.Logger:
+    log_format = '%(asctime)s - [%(levelname)s] %(name)s - %(message)s'
+    formatter = logging.Formatter(log_format)
+    
+    os.makedirs(log_dir, exist_ok=True)
+    
+    file_handler = RotatingFileHandler(
+        os.path.join(log_dir, 'monitor.log'),
+        maxBytes=10*1024*1024,
+        backupCount=5
+    )
+    file_handler.setFormatter(formatter)
+    
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+    
+    return logger
